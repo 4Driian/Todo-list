a4f380f6e76d0cbde2206b2c90eefbbd3d33215f
MelanieLopezAlfaro <107006490+MelanieLopAl@users.noreply.github.com>
2023-03-28 15:35:06 -0600

    Tiempo mostrado en pantalla (#2)

--- main.js
+++ main.js
@@ -27,3 +27,55 @@ class TodoList {
         this.addTask();
       }
     });
+
+setInterval(() => {
+      const taskItems = this.taskList.getElementsByTagName('li');
+      for (let i = 0; i < taskItems.length; i++) {
+        const taskItem = taskItems[i];
+        const timeElement = taskItem.querySelector('.time');
+        if (timeElement) {
+          const timestamp = parseInt(timeElement.getAttribute('data-timestamp'));
+          const timeDiff = Math.floor((Date.now() - timestamp) / 1000);
+          let timeString;
+          if (timeDiff < 60) {
+            timeString = timeDiff + ' segundos';
+          } else if (timeDiff < 3600) {
+            timeString = Math.floor(timeDiff / 60) + ' minutos';
+          } else {
+            timeString = Math.floor(timeDiff / 3600) + ' horas';
+          }
+          timeElement.innerText = timeString;
+        }
+      }
+    }, 1000);
+  }
+
+  addTask() {
+    const newTask = document.createElement('li');
+    const deleteBtn = document.createElement('button');
+    const checkbox = document.createElement('input');
+    const timeElement = document.createElement('span');
+    timeElement.className = 'time';
+    timeElement.setAttribute('data-timestamp', Date.now());
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', () => {
+      this.taskLine(checkbox);
+      this.textLine(checkbox, newTask);
+    });
+    deleteBtn.innerText = 'Eliminar';
+    newTask.innerText = this.inputTask.value;
+    if (this.selectedCategory) {
+      const category = document.createElement('span');
+      category.innerText = this.selectedCategory;
+      newTask.appendChild(category);
+      const categoryIcon = document.createElement('img');
+      categoryIcon.src = `img/${this.selectedCategory.toLowerCase()}.svg`;
+      newTask.appendChild(categoryIcon);
+      this.selectedCategory = null;
+    }
+    newTask.appendChild(timeElement);
+    newTask.appendChild(checkbox);
+    newTask.appendChild(deleteBtn);
+    this.taskList.appendChild(newTask);
+    this.inputTask.value = '';
+  }
